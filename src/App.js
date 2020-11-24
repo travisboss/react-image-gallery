501560ecef0a657a073dbb88d3efb8f99cfe364d
Travis Boss <travisboss@gmail.com>
2020-11-24 12:57:53 -0600

    add env setup fetch pixabay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [images, setImages] = useState([])
+  // set to true and have state change to false
+  const [isLoading, setIsLoading] = useState(true)
+  const [term, setTerm] = useState('')
+
+  // fetch data from pixabay using .env and term from state
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+      .then(data => {
+      setImages(data.hits)
+      setIsLoading(false)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
