b527e82f4c417fc42447014e3f64bec7aff68c9c
Travis Boss <travisboss@gmail.com>
2020-11-25 20:20:30 +0000

    added ImageSearch

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
       setIsLoading(false)
     })
     .catch(err => console.log(err))
-  }, [])
+  }, [term])
 
   return (
     <div className="container mx-auto">
-      <ImageSearch />
+      <ImageSearch searchText={(text) => setTerm(text)} />
 
 
       { isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading ...</h1> : <div className="grid grid-cols-3 gap-4">
